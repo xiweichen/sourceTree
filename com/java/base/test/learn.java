767075c7e42ffd12ce78dcd66126c9191d0ea883
xiweichen <1096035096@qq.com>
2017-07-13 21:18:02 +0800

    二恶而

--- com/java/base/test/learn.java
+++ com/java/base/test/learn.java
@@ -4,13 +4,14 @@ package com.java.base.test;
  * Created by chenxiwei1 on 2017/7/13.
  */
 public class learn {
-
     public learn(){
         System.out.println("Dev 中增加一个输出文件");
         System.out.println("find Red from  master主分支的一个分支点进行创建的");
-
         System.out.println("master 的本地分支");
+    }
 
+
+    public static void main(String[] args) {
 
     }
 
